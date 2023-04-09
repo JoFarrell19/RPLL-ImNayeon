d4d69ed75dda8c182bc562376f84beec7e276db4
Richard Leonardo <85978263+Rikat19@users.noreply.github.com>
2023-04-09 21:14:38 +0700

    updateStatus dan enum

--- Controller/UpdateStatusOrder.php
+++ Controller/UpdateStatusOrder.php
@@ -1,27 +1,37 @@
 <?php
-// Set up database connection
-$servername = "localhost";
-$username = "yourusername";
-$password = "yourpassword";
-$dbname = "yourdatabase";
+    session_start();
+    $con = mysqli_connect('localhost', 'root', '', 'punix');
+    $user_check=$_SESSION['iduser'];
+    $sql=mysqli_query($con,"select * from transactions where id_user='$user_check'");
+    $row=mysqli_fetch_array($sql,MYSQLI_ASSOC);
+    $loggedin_id=$row['id_user'];
+    $loggedin_email=$row['email'];
+    if(!isset($loggedin_id) || $loggedin_id==NULL) {
+        echo "<script type='text/javascript'>alert('Please login first.'); window.location.href='../Login_Register/login.html'</script>";
+    }
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Construct SQL query
-$sql = "UPDATE yourtable SET name='John Doe', email='john@example.com' WHERE id=1";
-
-// Execute query
-if ($conn->query($sql) === TRUE) {
-    echo "Record updated successfully";
-} else {
-    echo "Error updating record: " . $conn->error;
-}
-
-// Close connection
-$conn->close();
+    $status = "";
+    switch (StatusEnum.$status) {
+        case StatusEnum::OrderReceived:
+            echo "Order Received";
+            break;
+        case StatusEnum::InKitchen:
+            echo "Your Order in the Kitchen";
+            break;
+        case StatusEnum::OnTheWay:
+            echo "Your Order On The Way";
+            break;
+    }
+    
+    if (isset($_POST['submit'])) {
+        $status = mysqli_real_escape_string($con, $_POST['status']);
+        if (empty($status)) {
+            echo "<script type='text/javascript'>alert('Status still empty'); window.location.href='../EditProfile/EditProfile.php';</script>";
+            $safe = 0;
+        }
+        
+        $result = mysqli_query($con, $user_check_query);
+        $user = mysqli_fetch_assoc($result);
+        
+    }
 ?>
