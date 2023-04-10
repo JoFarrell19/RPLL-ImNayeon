93bb6cb56c0a49399ceacc65900f521d1e1f2180
Matthew Evans <85978388+MatthewEvansH@users.noreply.github.com>
2023-04-11 02:45:17 +0700

    Update dbcon.php

--- Controller/dbcon.php
+++ Controller/dbcon.php
@@ -7,9 +7,4 @@ $database = "punix";
 // Membuat koneksi
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-// Memeriksa koneksi
-if (!$conn) {
-    die("Connection Failed:  " . mysqli_connect_error());
-}
-echo "Connection Success";
 ?>
