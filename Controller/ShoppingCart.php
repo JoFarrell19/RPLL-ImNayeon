85ccb39cc123db649150b3a08e4e1450d8e4b74a
davechristianto <91650970+davechristianto@users.noreply.github.com>
2023-04-11 00:22:39 +0700

    Update ShoppingCart.php

--- Controller/ShoppingCart.php
+++ Controller/ShoppingCart.php
@@ -1,75 +1,30 @@
 <?php
+
 $servername = "localhost";
-$username = "root";
-$password = "";
-$database = "punix";
+$username = "username";
+$password = "password";
+$dbname = "punix";
 
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if (!$conn) {
-    die("Connection Failed: " . mysqli_connect_error());
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
 }
-echo "Connection Success";
 
-$id_menu = $_GET ["id_menu"];
-$quantity = $_GET ["quantity"];
-$user_id = $_SESSION["user_id"];
+
 $id_cart = $_GET ["id_cart"];
+$id_menu = $_GET ["id_menu"];
 
-$sql = "SELECT detailed_carts.id_detailed_cart, detailed_carts.id_cart, detailed_carts.id_menu, detailed_carts.quantity 
-	FROM detailed_carts 
-	JOIN carts 
-	ON detailed_carts.id_cart=carts.id_cart 
-	WHERE carts.id_user = $user_id";
-$stmt = $db->prepare($sql);
-$stmt->bind_param("i", $user_id);
-$stmt->execute();
-$result = $stmt->get_result();
+$sql = "INSERT INTO detailed_carts (id_cart, id_menu, quantity) VALUES ('$id_cart', $id_menu, 1 )";
 
-while ($row = $result->fetch_assoc()) {
-    $detailedCart = new stdClass();
-    $detailedCart->Id_Detailed_Cart = $row["id_detailed_cart"];
-    $detailedCart->Id_Cart = $row["id_cart"];
-    $detailedCart->Id_Menu = $row["id_menu"];
-    $detailedCart->Quantity = $row["quantity"];
-    $detailedCarts[] = $detailedCart;
+
+if ($conn->query($sql) === TRUE) {
+    echo "Success Insert";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-$isFound = false;
 
-for ($i = 0; $i < count($detailedCarts); $i++) {
-    if ($detailedCarts[$i]['id_menu'] == $id_menu_int) {
-        $query = "UPDATE detailed_carts SET quantity = quantity + ".$quantity." WHERE id_detailed_cart = ?";
-        $statement = $db->prepare($query);
-        $statement->bind_param('i', $detailedCarts[$i]['Id_Detailed_Cart']);
-        $statement->execute();
-        $isFound = true;
-        if ($statement->affected_rows > 0) {
-            http_response_code(200);
-            echo json_encode(array("message" => "Added To Cart"));
-        } else {
-            http_response_code(400);
-            echo json_encode(array("message" => "Failed"));
-        }
-        return;
-    }
-}
-
-if ($isFound == false) {
-    $query = "INSERT INTO detailed_carts(id_cart, id_menu, quantity) VALUES (?,?,?)";
-    $statement = $db->prepare($query);
-    $statement->bind_param('iii', $detailedCarts[0]['Id_Cart'], $id_menu_int, $quantity);
-    $statement->execute();
-    if ($statement->affected_rows > 0) {
-        http_response_code(200);
-        echo json_encode(array("message" => "Added To Cart"));
-    } else {
-        http_response_code(400);
-        echo json_encode(array("message" => "Failed"));
-    }
-}
-
-// Close connection
-mysqli_close($conn);
+$conn->close();
 ?>
