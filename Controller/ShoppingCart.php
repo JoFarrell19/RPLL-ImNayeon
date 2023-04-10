827ed4798d0e3f67735aa5b0918245f54d555ac7
davechristianto <91650970+davechristianto@users.noreply.github.com>
2023-04-10 21:31:03 +0700

    Update ShoppingCart.php

--- Controller/ShoppingCart.php
+++ Controller/ShoppingCart.php
@@ -68,26 +68,6 @@ if ($isFound == false) {
     }
 }
 
-$sql = "SELECT *
-FROM detailed_carts
-JOIN menu ON menu.id_menu = detailed_carts.id_menu
-JOIN carts ON carts.id_cart = detailed_cart.id_cart
-WHERE cart.id_user = $user_id";
-
-// Execute query
-$result = $conn->query($sql);
-
-// Display results
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "Name: " . $row["name"]. "<br>";
-        echo "Quantity: " . $row["quantity"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-
 // Close connection
 mysqli_close($conn);
 ?>
