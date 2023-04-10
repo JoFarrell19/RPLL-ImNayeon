ab87312902d61e3a6dc087ee600d15a2d2451373
Richard Leonardo <85978263+Rikat19@users.noreply.github.com>
2023-04-10 21:55:18 +0700

    fix shopping cart

--- Controller/ShoppingCart.php
+++ Controller/ShoppingCart.php
@@ -23,7 +23,7 @@ $sql = "SELECT detailed_carts.id_detailed_cart, detailed_carts.id_cart, detailed
 	ON detailed_carts.id_cart=carts.id_cart 
 	WHERE carts.id_user = $user_id";
 $stmt = $db->prepare($sql);
-$stmt->bind_param("i", GetOnlineUserId($r));
+$stmt->bind_param("i", $user_id);
 $stmt->execute();
 $result = $stmt->get_result();
 
